e90df4b722c747ff8b49042c0f3de61b4a231061
Michael Avagianos <hello@maikkun.dev>
2024-06-15 23:48:42 +0300

    fix: enable CORS; add root response

--- main.go
+++ main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/maikkundev/start-daily-todo/database"
 	"github.com/maikkundev/start-daily-todo/handlers"
 )
 
 func main() {
-	app := fiber.New()
+	var app = fiber.New()
+	app.Use(cors.New())
 
-	err := database.Connect()
+	var err = database.Connect()
 	if err != nil {
 		return
 	}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Welcome to StartDailyToDo")
+	})
 
 	app.Get("/todos", handlers.GetTodos)
 	app.Get("todos/:id", handlers.GetTodo)
