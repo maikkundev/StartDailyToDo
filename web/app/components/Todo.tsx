ed678ad9a81fc2ccc6ffb119d84fd56358f20ff4
Michael Avagianos <hello@maikkun.dev>
2024-08-06 18:49:29 +0300

    feat: stylize Todo

--- web/app/components/Todo.tsx
+++ web/app/components/Todo.tsx
@@ -1,28 +1,42 @@
-// components/Todo.tsx
 import React from "react";
 import { Todo } from "../interfaces/Todo";
+import { Checkbox, UnstyledButton, Text } from "@mantine/core";
+import classes from "./Navbar.module.css";
 
 interface TodoProps {
-    todos: Todo[];
+  todos: Todo[];
+  toggleTodo: (todoId: number, currentStatus: boolean) => Promise<void>;
 }
 
-const TodoList: React.FC<TodoProps> = ({ todos }) => {
-    return (
-        <div>
-            {todos.length > 0 ? (
-                todos.map((todo) => (
-                    <ol key={todo.ID}>
-                        <li>
-                            {todo.name} <button>x</button>
-                        </li>
-                        <li>{todo.isDone.toString()}</li>
-                    </ol>
-                ))
-            ) : (
-                <p>Loading...</p>
-            )}
-        </div>
-    );
+const TodoList: React.FC<TodoProps> = ({ todos, toggleTodo }) => {
+  return (
+    <div>
+      {todos.length > 0 ? (
+        todos.map((todo) => (
+          <ol key={todo.ID}>
+            <li>
+              <UnstyledButton
+                onClick={() => toggleTodo(todo.ID, todo.IsDone)}
+                className={classes.button}
+              >
+                <Checkbox
+                  checked={todo.IsDone || false}
+                  tabIndex={-1}
+                  size="md"
+                  mr="xl"
+                  styles={{ input: { cursor: "pointer" } }}
+                  aria-hidden
+                ></Checkbox>
+                <Text>{todo.Name}</Text>
+              </UnstyledButton>
+            </li>
+          </ol>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default TodoList;
